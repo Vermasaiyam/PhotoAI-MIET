3e6d14b9b545eae98b10f07dd141ebeee7100518
Vermasaiyam <vermasaiyam9@gmail.com>
2025-03-01 11:39:34 +0530

    backend start

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-app.use('/api/user', userRoutes);
+// app.use('/api/user', userRoutes);
 // app.use('/api/video', videoRoutes);
 // app.use('/api/question', questionRoutes);
 
