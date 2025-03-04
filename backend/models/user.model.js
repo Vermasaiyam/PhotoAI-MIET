3a08476f29e0eb7aae9eff78d6a544bf85a7d931
Vermasaiyam <vermasaiyam9@gmail.com>
2025-03-04 11:18:11 +0530

    user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    profilePicture: { type: String, default: '' },
-    faceEncoding: { type: String, default: '', unique: true },
-    groups: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Group' }],
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+    {
+        username: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        profilePicture: { type: String, default: "" },
+        faceEncoding: {
+            type: [Number],
+            required: true,
+        },
+        groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group" }],
+        createdGroups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group" }],
+    },
+    { timestamps: true }
+);
 
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model("User", userSchema);
