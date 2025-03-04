572a50c00d8c83f9eb68a8b7fe7d74dca2325033
Vermasaiyam <vermasaiyam9@gmail.com>
2025-03-04 11:19:02 +0530

    user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,17 +1,34 @@
 import express from "express";
-import { changePassword, deleteAccount, editProfile, getProfile, login, logout, register, userData } from "../controllers/user.controller.js";
-import Authenticated from "../midllewares/Authenticated.js";
-import upload from "../midllewares/multer.js";
+import {
+    changePassword,
+    deleteAccount,
+    editProfile,
+    getFullUser,
+    getProfile,
+    getUserGroups,
+    login,
+    logout,
+    register,
+    storeFaceEncoding,
+    userData,
+} from "../controllers/user.controller.js";
+import Authenticated from "../middlewares/Authenticated.js";
+import upload from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.route('/register').post(register, upload.single("photo"));
-router.route('/login').post(login);
-router.route('/:id/profile').get(Authenticated, getProfile);
-router.route('/profile/edit').post(Authenticated, upload.single('profilePicture'), editProfile);
-router.route('/change-password').post(Authenticated, changePassword);
-router.route('/logout').get(logout);
-router.route('/userData/:id').get(userData);
-router.route('/delete/:userId').delete(deleteAccount);
+router.route("/register").post(upload.single("photo"), register);
+router.route("/store-face-encoding").post(storeFaceEncoding);
+router.route("/login").post(login);
+router.route("/:id/profile").get(Authenticated, getProfile);
+router
+    .route("/profile/edit")
+    .post(Authenticated, upload.single("profilePicture"), editProfile);
+router.route("/change-password").post(Authenticated, changePassword);
+router.route("/logout").get(logout);
+router.route("/userData/:id").get(userData);
+router.route("/groups").get(getUserGroups);
+router.route("/delete/:userId").delete(deleteAccount);
+router.route("/:userId").get(getFullUser);
 
 export default router;
